ff3f5095735a676cbbb5ab51b884acba6520ca62
Hardik Vala <hardikvala24@gmail.com>
2021-05-11 21:59:32 -0700

    Encrypt programming assignment solution for Week 1 of Cryptography I | coursera

---
+++ CryptographyICoursera/Week1/decrypt_prog_assign_soln.py
@@ -0,0 +1,28 @@
+import argparse
+import binascii
+import math
+
+
+def main():
+	passphrase = b"SOLUTION TEXT GOES HERE"
+	encrypted_prog_assign_soln_input_path = "prog_assign_soln.encrypted"
+	decrypted_prog_assign_soln_out_path = "prog_assign_soln.py"
+
+	key_hex = binascii.hexlify(passphrase)
+	with open(encrypted_prog_assign_soln_input_path, 'r') as f:
+		ciphertext_hex = f.read()
+
+	expanded_key_hex_prefix = key_hex * int(math.floor(len(ciphertext_hex) / float(len(key_hex))))
+	expanded_key_hex = expanded_key_hex_prefix + key_hex[:(len(ciphertext_hex) - len(expanded_key_hex_prefix))]
+
+	print(len(expanded_key_hex))
+	print(len(ciphertext_hex))
+	plaintext_hex = "0" + hex(int(ciphertext_hex, 16) ^ int(expanded_key_hex, 16))[2:]
+	plaintext = bytes.fromhex(plaintext_hex).decode()
+
+	with open(decrypted_prog_assign_soln_out_path, 'w') as f:
+		f.write(plaintext)
+
+
+if __name__ == '__main__':
+	main()
